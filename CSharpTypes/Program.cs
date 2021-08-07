e1bea92f9dd9faeb7671c17cb0f09adfd6f6fd18
hebraheem <iadebiyi01.com>
2021-08-07 19:37:40 +0100

    initialize types in c-sharp

---
+++ CSharpTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
